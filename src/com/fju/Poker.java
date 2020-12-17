e6000ba7f006180dc1257b72e3f33d48e0a44d09
Roro <rorochen0903@gmail.com>
2020-12-17 22:25:24 +0800

    update java

--- src/com/fju/Poker.java
+++ src/com/fju/Poker.java
@@ -3,7 +3,8 @@ package com.fju;
 public class Poker {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PokerCard card = new PokerCard(28);
+		System.out.println(card.value);
 
 	}
 
--- src/com/fju/PokerCard.java
+++ src/com/fju/PokerCard.java
@@ -1,5 +1,12 @@
 package com.fju;
 
 public class PokerCard {
+	int value;
+	char type;
+	int point;
+	
+	public PokerCard(int value) {
+		this.value = value;
+	}
 
 }

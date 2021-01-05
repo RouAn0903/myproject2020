5205e93a443dbbb51c27b3c3372ff4e3983bb9a1
Roro <rorochen0903@gmail.com>
2021-01-05 16:34:54 +0800

    add TestSwitch & d1

--- src/com/fju/Student.java
+++ src/com/fju/Student.java
@@ -5,6 +5,30 @@ public class Student {
 	int english;
 	int math;
 	
+	public String toString() {
+		int avg = average();
+		String grading = "F";
+		switch(avg/10) {
+		case 10:
+		case 9:
+			grading = "A";
+			break;
+		case 8:
+			grading = "B";
+			break;
+		case 7:
+			grading = "C";
+			break;
+		case 6:
+			grading = "D";
+			break;
+		default:
+			grading = "F";
+			break;
+		}
+		return grading;
+	}
+	
 	public Student(String n, int english, int math) {
 		this.name = n;
 		this.english=english;
@@ -13,8 +37,9 @@ public class Student {
 	public int average() {
 		return (english+math)/2;
 	}
+	
 	public void printscore() {
-		System.out.println(name + "\t" + english + "\t" + math +"\t" +average());
+		System.out.println(name + "\t" + english + "\t" + math +"\t" +average()+"\t" +toString());
 	}
 
 }
---
+++ src/com/fju/TestSwitch.java
@@ -0,0 +1,21 @@
+package com.fju;
+
+public class TestSwitch {
+
+	public static void main(String[] args) {
+		int n = 1;
+		switch (n) {
+		case 1:
+			System.out.println("1");
+			
+		case 2:
+			System.out.println("2");
+			
+		case 3:
+			System.out.println("3");
+		}
+		System.out.println("hello");
+
+	}
+
+}
